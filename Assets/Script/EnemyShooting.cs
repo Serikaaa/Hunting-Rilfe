f1912cf391555fae75571d0a915f4719856c7226
Submadiq <minecraftmod6a2@gmail.com>
2023-05-31 17:02:01 +0700

    Them enemy

--- Assets/Script/EnemyShooting.cs
+++ Assets/Script/EnemyShooting.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
-public class Shooting : NetworkBehaviour
+public class EnemyShooting : NetworkBehaviour
 {
     public Transform firePoint1;
     public GameObject bulletPrefab;
@@ -22,7 +22,6 @@ public class Shooting : NetworkBehaviour
     void Shoot()
     {
         GameObject bullet1 = Instantiate(bulletPrefab, firePoint1.position, firePoint1.rotation);
-
         Rigidbody2D rb = bullet1.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint1.up * bulletForce, ForceMode2D.Impulse);
         bullet1.GetComponent<NetworkObject>().Spawn();
