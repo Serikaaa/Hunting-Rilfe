2f5b798f39f214b9e1fec4d1bf36a397f63f211c
Le Vi <vianh.le1310@gmail.com>
2023-06-04 00:26:09 +0700

    Login and Register screen

---
+++ Assets/Script/LoginLogoutRegister.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class LoginLogoutRegister : MonoBehaviour
+{
+    string baseUrl = "http://localhost/Hunting_Rifle_database/";
+    public InputField accountUserName;
+    public InputField accountPassword;
+    public Text info;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AccountRegister()
+    {
+        string uName = accountUserName.text;
+        string pWord = accountPassword.text;
+        StartCoroutine(RegisterNewAccount(uName, pWord));
+    }
+    public void AccountLogin()
+    {
+        string uName = accountUserName.text;
+        string pWord = accountPassword.text;
+        StartCoroutine(LogInAccount(uName, pWord));
+    }
+    IEnumerator RegisterNewAccount(string uName, string pWord)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("newAccountUsername", uName);
+        form.AddField("newAccountPassword", pWord);
+        using (UnityWebRequest www = UnityWebRequest.Post(baseUrl, form))
+        {
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string responseText = www.downloadHandler.text;
+                Debug.Log("Response = " + responseText);
+                info.text = "Response = " + responseText;
+            }
+        }
+    }
+    IEnumerator LogInAccount(string uName, string pWord)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUsername", uName);
+        form.AddField("loginPassword", pWord);
+        using (UnityWebRequest www = UnityWebRequest.Post(baseUrl, form))
+        {
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string responseText = www.downloadHandler.text;
+                Debug.Log("Response = " + responseText);
+                info.text = "Response = " + responseText;
+            }
+        }
+    }
+}
